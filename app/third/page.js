adf1473ee9a0983bb6caed64490a77a4b7f6f651
FatihG34 <fthgnydn34@gmail.com>
2023-06-01 11:07:13 +0300

    Star-Wars-SpaceShip added

--- app/third/page.js
+++ app/third/page.js
@@ -4,12 +4,15 @@ import { Canvas } from '@react-three/fiber'
 import { OrbitControls, Stars } from '@react-three/drei';
 import Box from '@/components/box/Box';
 import Sphered from '@/components/sphere/Sphere';
-import Starwars from "../../components/starWars/Starwars";
+import Starwars2 from "@/components/Starwars2";
+import StarWars3 from '@/components/starWars/StarWars3';
+
+// import Starwars from "../../components/starWars/Starwars";
 // import { GLTFLoader } from '../js/examples/jsm/loaders/GLTFLoader.js';
 
 const Third = () => {
     return (
-        <div className='h-screen bg-black'>
+        <div className='bg-black'>
             <div className='h-96'>
                 <Canvas>
                     <pointLight position={[10, 10, 10]} />
@@ -41,15 +44,31 @@ const Third = () => {
                     <Stars />
                 </Canvas>
             </div>
-            {/* <Canvas>
-                <OrbitControls enableZoom={false} />
-                <ambientLight intensity={0.5} />
-                <directionalLight position={[-2, 5, 2]} intensity={1} />
-                <Suspense fallback={null}>
-                    <Starwars />
-                </Suspense>
-                <Stars />
-            </Canvas> */}
+            <div className='h-screen'>
+                <Canvas>
+                    <OrbitControls />
+                    <ambientLight intensity={0.5} />
+                    <directionalLight position={[-2, 5, 2]} intensity={1} />
+                    <Suspense fallback={null}>
+                        <Starwars2 />
+                    </Suspense>
+                    {/* <Stars /> */}
+                    {/* <Stars /> */}
+                    {/* <Stars /> */}
+
+                </Canvas>
+            </div>
+            <div className='h-screen'>
+                <Canvas>
+                    <OrbitControls />
+                    <ambientLight intensity={0.5} />
+                    <directionalLight position={[-2, 5, 2]} intensity={1} />
+                    <Suspense fallback={null}>
+
+                        <StarWars3 />
+                    </Suspense>
+                </Canvas>
+            </div>
         </div>
     )
 }
